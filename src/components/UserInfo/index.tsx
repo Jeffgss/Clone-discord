89363ae0baae0e2ae03b20d6b39d9441dcc07663
Jeff <jefferson.santos@maxmilhas.com.br>
2020-07-06 09:57:19 -0300

    Finalizing third component

---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import {
+  Container,
+  Profile,
+  Avatar,
+  UserData,
+  Icons,
+  MicIcon,
+  HeadphoneIcon,
+  SettingsIcon
+} from "./styles";
+
+const UserInfo: React.FC = () => {
+  return (
+    <Container>
+      <Profile>
+        <Avatar />
+
+        <UserData>
+          <strong>Jeff Santos</strong>
+          <span>#0000</span>
+        </UserData>
+      </Profile>
+
+      <Icons>
+        <MicIcon />
+        <HeadphoneIcon />
+        <SettingsIcon />
+      </Icons>
+    </Container>
+  );
+};
+
+export default UserInfo;
